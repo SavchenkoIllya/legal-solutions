072ca220a136db7d332fbbbae5baf503a8bcd58d
ilya <savchik0o@gmail.com>
2024-07-01 00:55:24 +0200

    <rework>: telegram server reworks using tg hooks and api/interfaces/telegram endpath

--- app/api/interfaces/telegram/route.ts
+++ app/api/interfaces/telegram/route.ts
@@ -1,16 +1,53 @@
 import { sql } from "@vercel/postgres";
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export async function POST(req: Request) {
-    const { chatId, username } = await req.json();
+const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_KEY || ""
 
-    try {
-        await sql`INSERT INTO telegram_admins (username, chat_id)
-                  VALUES (${username}, ${chatId})
-                  ON CONFLICT (username) DO NOTHING;`;
-        return Response.json({ success: true });
-    } catch (error) {
-        console.error(error);
-        throw new Error("Something went wrong writing new telegram_admin");
+// export async function POST(req: Request) {
+//     const { chatId, username } = await req.json();
+
+//     try {
+//         await sql`INSERT INTO telegram_admins (username, chat_id)
+//                   VALUES (${username}, ${chatId})
+//                   ON CONFLICT (username) DO NOTHING;`;
+//         return Response.json({ success: true });
+//     } catch (error) {
+//         console.error(error);
+//         throw new Error("Something went wrong writing new telegram_admin");
+//     }
+
+// }
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { body } = req;
+
+    // Обработка входящего сообщения от Telegram
+    if (body.message) {
+        const { id, username } = body.message.chat
+        const chatId: number = body.message.chat.id;
+        const text: string = 'You have been successfully subscribed!';
+        try {
+            await sql`INSERT INTO telegram_admins (username, chat_id)
+                      VALUES (${username}, ${id})
+                      ON CONFLICT (username) DO NOTHING;`;
+        } catch (error) {
+            return res.status(400);
+        }
+
+        // Отправка сообщения обратно через Telegram API
+        await fetch(`https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/sendMessage`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: text,
+            }),
+        });
+
+        return res.status(200).json({ status: 'ok' });
+    } else {
+        return res.status(200).json({ status: 'no message' });
     }
-
 }
